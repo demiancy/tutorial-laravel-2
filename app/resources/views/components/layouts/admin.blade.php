adae1218602ad3cb909d57669cd242c97c8f2204
cyia <demiancy.dev@gmail.com>
2022-05-10 13:20:07 -0300

    Added logout component

--- app/resources/views/components/layouts/admin.blade.php
+++ app/resources/views/components/layouts/admin.blade.php
@@ -1,7 +1,7 @@
 <x-layouts.base>
     <body class="font-sans antialiased">
         <div class="flex-col w-full md:flex md:flex-row md:min-h-screen">
-            <x-admin.nav />
+            <x-admin.nav.nav />
 
             <!-- Page Content -->
             <main class="m-2 p-8 w-full">
