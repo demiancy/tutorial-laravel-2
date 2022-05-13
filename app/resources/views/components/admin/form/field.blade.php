a45e7053a866cf78048a105b8c78a14980cc0a6e
cyia <demiancy.dev@gmail.com>
2022-05-13 14:33:13 -0300

    Fix bug in admin field component

--- app/resources/views/components/admin/form/field.blade.php
+++ app/resources/views/components/admin/form/field.blade.php
@@ -3,11 +3,14 @@
     'type'  => 'text'
 ])
 
+@php
+    $errorClass = $errors->has($field) ? 'border-red-400' : '';
+@endphp
+
 <input 
-    {!! $attributes !!}
+    {!! $attributes->merge(['class' => "block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5 $errorClass"]) !!}
     type="{{ $type }}" 
     id="{{ $field }}" 
-    name="{{ $field }}"
-    class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5 @error($field) border-red-400 @enderror" 
+    name="{{ $field }}" 
 />
 
