33bc89d75d21b31d2aae8c0a38783da5f33b430a
cyia <demiancy.dev@gmail.com>
2022-06-04 20:04:02 -0300

    Added the permissions to seeder

--- app/database/seeders/RoleSeeder.php
+++ app/database/seeders/RoleSeeder.php
@@ -22,11 +22,51 @@ class RoleSeeder extends Seeder
         $role = Role::create(['name' => 'admin']);
         $role->givePermissionTo([
             'view_admin',
+            'view_categories',
+            'update_categories',
+            'create_categories',
+            'delete_categories',
+            'view_menus',
+            'update_menus',
+            'create_menus',
+            'delete_menus',
+            'view_tables',
+            'update_tables',
+            'create_tables',
+            'delete_tables',
+            'view_reservations',
+            'update_reservations',
+            'create_reservations',
+            'delete_reservations',
+            'view_users',
+            'update_users',
+            'create_users',
+            'delete_users',
+            'view_roles',
+            'update_roles',
+            'create_roles',
+            'delete_roles',
         ]);
 
         $role = Role::create(['name' => 'operator']);
         $role->givePermissionTo([
             'view_admin',
+            'view_categories',
+            'update_categories',
+            'create_categories',
+            'delete_categories',
+            'view_menus',
+            'update_menus',
+            'create_menus',
+            'delete_menus',
+            'view_tables',
+            'update_tables',
+            'create_tables',
+            'delete_tables',
+            'view_reservations',
+            'update_reservations',
+            'create_reservations',
+            'delete_reservations',
         ]);
     }
 }
